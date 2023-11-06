d2ff7ed47e9dbfc5a9dff41283c316e24682fa82
Hamza Ahmed <hamzaahmed@Hamzas-Mac-mini.local>
2023-11-06 09:02:07 +0800

    add all files

---
+++ apps/reservations/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { ReservationsModule } from './reservations.module';
+import { Logger } from 'nestjs-pino'
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  console.log("hello")
+  const app = await NestFactory.create(ReservationsModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger))
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
+}
+bootstrap();
